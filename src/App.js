97bbf6350c3902dbacb7154b670b06dec194c782
Harshal73 <harshal73gh@gmail.com>
2022-11-25 23:52:01 +0530

    products

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import About from "./components/About";
 import Home from "./components/Home";
 import { Last } from "./components/Last";
 import Navbar from "./components/Navbar";
+import Products from "./components/Products";
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
      <Navbar/>
      <Home/>
      <About/>
+     <Products/>
      <Last/>  
   
     </div>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -14,25 +14,28 @@ const Home = () => {
           <div className='flex flex-col justify-center h-full ' >
 
           <h2 className='text-4xl sm:text-7xl font-bold text-white' >We are group of Engineers and traders</h2>
-          <p>
+          <p className='text-gray-300 py-4 max-w-md'>
             We have wide range of trading softwares for affordable price.
             Those are adavanced tools which help you to trade better with  help of 
             platform which connected with realtime datafeed, it provides market 
             transparency for user..
             
           </p>
-          <button>
+          <div>
+          <button className='group text-white w-fit px-6 py-3 my-2 flex items-center
+           rounded-md bg-gradient-to-r from bg-cyan-500 to-blue-500 cursor-pointer '>
             Products 
-            <span>
-                <MdOutlineKeyboardArrowRight/>
+            <span className='group-hover:rotate-90 duration-300' >
+                <MdOutlineKeyboardArrowRight size={25} className='ml-1' />
             </span>
           
           </button>
-
+          </div>
         </div>
         <div>
-        </div>
-        <img src={HeroImage}  alt='bateman' className='rounded-2xl mx-auto min-w-min md:w-full ' />
+       
+        <img src={HeroImage}  alt='bateman' className=' rounded-2xl w-2/3 mx-auto  md:w-full ' />
+      </div>
       </div>
       </div>
    
